6ae0fbc482de35cd3c38b007cee23bbc747aa602
John Joseph <atiquajohn@gmail.com>
2020-04-27 10:48:54 +0700

    added immer to the reducer

--- app/Main.js
+++ app/Main.js
@@ -1,6 +1,7 @@
 import React, { useState, useReducer } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { useImmerReducer } from 'use-immer'
 import StateContext from './StateContext'
 import DispatchContext from './DispatchContext'
 
@@ -24,18 +25,21 @@ function Main() {
 		flashMessages: [],
 	}
 
-	function ourReducer(state, action) {
+	function ourReducer(draft, action) {
 		switch (action.type) {
 			case 'login':
-				return { loggedIn: true, flashMessages: state.flashMessages }
+				draft.loggedIn = true
+				return
 			case 'logout':
-				return { loggedIn: false, flashMessages: state.flashMessages }
+				draft.loggedIn = false
+				return
 			case 'flashMessage':
-				return { loggedIn: state.loggedIn, flashMessages: state.flashMessages.concat(action.value) }
+				draft.flashMessages.push(action.value)
+				return
 		}
 	}
 
-	const [state, dispatch] = useReducer(ourReducer, initialState)
+	const [state, dispatch] = useImmerReducer(ourReducer, initialState)
 
 	return (
 		<StateContext.Provider value={state}>
