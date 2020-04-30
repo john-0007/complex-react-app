006c4a46c7f746a55dad746d6e392fb2661c7258
John Joseph <atiquajohn@gmail.com>
2020-05-01 03:06:25 +0700

    added search overlay

--- app/Main.js
+++ app/Main.js
@@ -21,6 +21,7 @@ import FlashMessages from './components/FlashMessages'
 import Profile from './components/Profile'
 import EditPost from './components/EditPost'
 import NotFound from './components/NotFound'
+import Search from './components/Search'
 
 function Main() {
 	const initialState = {
@@ -31,6 +32,7 @@ function Main() {
 			username: localStorage.getItem('complexappUsername'),
 			avatar: localStorage.getItem('complexappAvatar'),
 		},
+		isSearchOpen: false,
 	}
 
 	function ourReducer(draft, action) {
@@ -44,6 +46,13 @@ function Main() {
 				return
 			case 'flashMessage':
 				draft.flashMessages.push(action.value)
+				return
+
+			case 'openSearch':
+				draft.isSearchOpen = true
+				return
+			case 'closeSearch':
+				draft.isSearchOpen = false
 				return
 		}
 	}
@@ -94,6 +103,7 @@ function Main() {
 							<NotFound />
 						</Route>
 					</Switch>
+					{state.isSearchOpen ? <Search /> : ''}
 					<Footer />
 				</BrowserRouter>
 			</DispatchContext.Provider>
