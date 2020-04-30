a605993cfbcabb2de5666717df52af678d3a97cf
John Joseph <atiquajohn@gmail.com>
2020-05-01 02:44:09 +0700

    write delete post functionalities

--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -1,16 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import Page from './Page'
-import { useParams, Link } from 'react-router-dom'
+import { useParams, Link, Redirect } from 'react-router-dom'
 import Axios from 'axios'
 import LoadingDotsIcon from './LoadingDotsIcon'
 import ReactMarkdown from 'react-markdown'
 import ReactTooltip from 'react-tooltip'
 import NotFound from './NotFound'
+import StateContext from '../StateContext'
+import DispatchContext from '../DispatchContext'
 
 function ViewSinglePost() {
 	const { id } = useParams()
 	const [isLoading, setIsLoading] = useState(true)
 	const [post, setPost] = useState()
+	const appState = useContext(StateContext)
+	const appDispatch = useContext(DispatchContext)
+	const [deleteAttemptCount, setDeleteAttemptCount] = useState(0)
+	const [deleteWasSuccessful, setDeleteWasSuccessful] = useState(false)
 
 	useEffect(() => {
 		const ourRequest = Axios.CancelToken.source()
@@ -30,6 +36,40 @@ function ViewSinglePost() {
 		}
 	}, [])
 
+	useEffect(() => {
+		if (deleteAttemptCount) {
+			const ourRequest = Axios.CancelToken.source()
+
+			async function deletePost() {
+				try {
+					const response = await Axios.delete(
+						`/post/${id}`,
+						{
+							data: {
+								token: appState.user.token,
+							},
+						},
+						{ cancelToken: ourRequest.token }
+					)
+					if (response.data == 'Success') {
+						setDeleteWasSuccessful(true)
+					}
+				} catch (e) {
+					console.log('There was a problem or the request was cancelled.')
+				}
+			}
+			deletePost()
+			return () => {
+				ourRequest.cancel()
+			}
+		}
+	}, [deleteAttemptCount])
+
+	if (deleteWasSuccessful) {
+		appDispatch({ type: 'flashMessage', value: 'Post was succefully delelted' })
+		return <Redirect to={`/profile/${appState.user.username}`} />
+	}
+
 	if (!isLoading && !post) {
 		return <NotFound />
 	}
@@ -44,20 +84,37 @@ function ViewSinglePost() {
 	const date = new Date(post.createdDate)
 	const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
 
+	function isOwner() {
+		if (appState.loggedIn) {
+			return appState.user.username == post.author.username
+		}
+
+		return false
+	}
+
+	function deleteHandler() {
+		const areYouSure = window.confirm('Do you really wnat to delete this post?')
+		if (areYouSure) {
+			setDeleteAttemptCount((prev) => prev + 1)
+		}
+	}
+
 	return (
 		<Page title={post.title}>
 			<div className='d-flex justify-content-between'>
 				<h2>{post.title}</h2>
-				<span className='pt-2'>
-					<Link to={`/post/${post._id}/edit`} data-tip='Edit' data-for='edit' className='text-primary mr-2'>
-						<i className='fas fa-edit'></i>
-					</Link>
-					<ReactTooltip id='edit' className='custom-tooltip' />{' '}
-					<a data-tip='Delete' data-for='delete' className='delete-post-button text-danger'>
-						<i className='fas fa-trash'></i>
-					</a>
-					<ReactTooltip id='delete' className='custom-tooltip' />
-				</span>
+				{isOwner() && (
+					<span className='pt-2'>
+						<Link to={`/post/${post._id}/edit`} data-tip='Edit' data-for='edit' className='text-primary mr-2'>
+							<i className='fas fa-edit'></i>
+						</Link>
+						<ReactTooltip id='edit' className='custom-tooltip' />{' '}
+						<a onClick={deleteHandler} data-tip='Delete' data-for='delete' className='delete-post-button text-danger'>
+							<i className='fas fa-trash'></i>
+						</a>
+						<ReactTooltip id='delete' className='custom-tooltip' />
+					</span>
+				)}
 			</div>
 
 			<p className='text-muted small mb-4'>
